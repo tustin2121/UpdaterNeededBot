d1ae5dc01ae5985e954ca601d7eac644d2726658
tustin2121 <tustin2121@gmail.com>
2018-04-12 18:42:44 -0400

    Body features

---
+++ data/extdat/bodyfeats.js
@@ -0,0 +1,21 @@
+// bodyfeats.js
+// A list of all pokemon by index number, and the body features they have.
+
+const cats = {
+	'head-only'		: {},
+	'head-legs'		: {},
+	'head-arms'		: {},
+	'head-base'		: {},
+	'wings'			: {},
+	'multi-wings'	: {},
+	'bipedal-tail'	: {},
+	'bipedal'		: {},
+	'quadruped'		: {},
+	'fins'			: {},
+	'insect'		: {},
+	'multi-body'	: {},
+	'tentacles'		: {},
+	'serpentine'	: {},
+};
+
+//TODO
